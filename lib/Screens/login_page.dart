0fd7bc0a50816d7b4bf09db09869cd617bee3337
Ashwini <ashwiniswami73@gmail.com>
2021-06-06 02:33:46 -0700

    Day4 completed

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -6,10 +6,54 @@ class Loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [
-        Image.asset("assets/images/login.png"),
-      ],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome to login Page",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: " Enter username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: " Enter password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hello This is Ashwini");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
